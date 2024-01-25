a76cdb12476e46f45fdee05d0b018ca0f8727918
cmkoi1 <cmkoi1003@gmail.com>
2024-01-25 17:58:11 +0900

    Feat: massageApi 완료 followApi 진행 중

---
+++ apps/team-1/src/utils/api/followApi.ts
@@ -0,0 +1,17 @@
+import { pb } from ".";
+
+export const handleGetfollow = async () =>
+  await pb.collection("follow").getFullList();
+
+export const handleGetfollower = async (id: string) => {
+  return await pb
+    .collection("follow")
+    .getFullList({ filter: `followingId='${id}'`, expand: 'followerId, followingId' });
+};
+
+export const handleGetfollowing = async (id: string) => {
+  return await pb
+    .collection("follow")
+    .getFullList({ filter: `followerId='${id}'`, expand: 'followerId, followingId' });
+};
+
