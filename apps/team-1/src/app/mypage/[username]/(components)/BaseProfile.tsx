31323d5137deb5c30393d9b7d0d6fbd11d6da28d
cmkoi1 <cmkoi1003@gmail.com>
2024-02-06 14:31:07 +0900

    Feat: 마이페이지 내가 쓴 댓글

--- apps/team-1/src/app/mypage/[username]/(components)/BaseProfile.tsx
+++ apps/team-1/src/app/mypage/[username]/(components)/BaseProfile.tsx
@@ -2,7 +2,7 @@ import {
   handleFollowerGetByUserId,
   handleFollowingGetByUserId,
 } from "@/utils/api";
-import { useRouter } from 'next/navigation';
+import { useRouter } from "next/navigation";
 
 interface baseProfileProps {
   userId: string;
@@ -36,8 +36,6 @@ interface baseProfileProps {
   };
 }
 
-
-
 const parseData = (data: { [index: string]: string }) => {
   return Object.entries(data);
 };
@@ -48,7 +46,7 @@ const parseArrayData = (data: {
   return Object.entries(data);
 };
 
-const reduceCareers = () => { };
+const reduceCareers = () => {};
 
 const BaseProfile = async ({
   userId,
@@ -69,8 +67,6 @@ const BaseProfile = async ({
   const parsedSkills = skills ? parseData(skills) : [];
   console.log(parsedCareers);
 
-
-
   return (
     <div>
       <div className="w-[334px] min-h-[1107px] bg-background-blue rounded-[10px] mx-auto grid place-items-center">
@@ -80,20 +76,30 @@ const BaseProfile = async ({
             alt="이미지를 설정"
             className="w-[80px] h-[80px] rounded-full"
           />
-          <p className="text-neutral-100 text-[20px] font-semibold">{userName}</p>
-          <p className="text-neutral-70 text-[14px] font-regular">{description || "자기 소개를 입력해 주세요."}</p>
+          <p className="text-neutral-100 text-[20px] font-semibold">
+            {userName}
+          </p>
+          <p className="text-neutral-70 text-[14px] font-regular">
+            {description || "자기 소개를 입력해 주세요."}
+          </p>
           <p className="text-neutral-30 text-[12px] font-medium">{email}</p>
         </div>
 
-        <button className="w-[304px] h-[40px] text-neutral-0 text-[14px] font-semibold bg-primary-80 rounded-[10px]">프로필 편집</button>
+        <button className="w-[304px] h-[40px] text-neutral-0 text-[14px] font-semibold bg-primary-80 rounded-[10px]">
+          프로필 편집
+        </button>
         <div className="w-[304px] min-h-[90px] bg-neutral-0 rounded-[10px]">
           <p className="text-primary-100 text-[20px] font-semibold">
             {followingData.length}
-            <span className="text-neutral-40 text-[16px] font-semibold">팔로우</span>
+            <span className="text-neutral-40 text-[16px] font-semibold">
+              팔로우
+            </span>
           </p>
           <p className="text-primary-100 text-[20px] font-semibold">
             {followerData.length}
-            <span className="text-neutral-40 text-[16px] font-semibold">팔로워</span>
+            <span className="text-neutral-40 text-[16px] font-semibold">
+              팔로워
+            </span>
           </p>
         </div>
         <div className="w-[304px] min-h-[332px] bg-neutral-0 rounded-[10px]">
@@ -101,53 +107,73 @@ const BaseProfile = async ({
             <p className="text-neutral-90 text-[16px] font-semibold">경력</p>
             <p className="text-primary-100 text-[24px] font-semibold">
               {parsedCareers.length}
-              <span className="text-neutral-70 text-[14px] font-regular">경력 연차</span>
+              <span className="text-neutral-70 text-[14px] font-regular">
+                경력 연차
+              </span>
             </p>
             <p className="text-primary-100 text-[24px] font-semibold">
               {parsedContest.length}
-              <span className="text-neutral-70 text-[14px] font-regular">공모전 입상</span>
+              <span className="text-neutral-70 text-[14px] font-regular">
+                공모전 입상
+              </span>
             </p>
             <p className="text-primary-100 text-[24px] font-semibold">
               {parsedSkills.length}
-              <span className="text-neutral-70 text-[14px] font-regular">기술 보유</span>
+              <span className="text-neutral-70 text-[14px] font-regular">
+                기술 보유
+              </span>
             </p>
             <div className="flex gap-2">
               {parsedSkills.map(item => (
                 <span
                   className="h-[21px] text-primary-100 text-[12px] font-medium border-primary-100 rounded-[100px]"
                   key={item[0]}
-                >{item[1]}</span>
+                >
+                  {item[1]}
+                </span>
               ))}
             </div>
           </div>
           <div>
-            <p className="text-neutral-90 text-[16px] font-semibold">SNS 연동</p>
+            <p className="text-neutral-90 text-[16px] font-semibold">
+              SNS 연동
+            </p>
             <div></div>
           </div>
         </div>
         <div className="w-[304px] min-h-[197px] bg-neutral-0 rounded-[10px]">
           <p className="text-neutral-90 text-[16px] font-semibold">나의 활동</p>
           <div>
-            <span className="text-neutral-70 text-[14px] font-regular">관심 로그</span>
+            <span className="text-neutral-70 text-[14px] font-regular">
+              관심 로그
+            </span>
             <button>ㄱ</button>
           </div>
           <div>
-            <span className="text-neutral-70 text-[14px] font-regular">최근 본 로그</span>
+            <span className="text-neutral-70 text-[14px] font-regular">
+              최근 본 로그
+            </span>
             <button>ㄱ</button>
           </div>
           <div>
-            <span className="text-neutral-70 text-[14px] font-regular">내가 쓴 댓글</span>
-            <button >ㄱ</button>
+            <span className="text-neutral-70 text-[14px] font-regular">
+              내가 쓴 댓글
+            </span>
+            <button>ㄱ</button>
           </div>
         </div>
         <div className="w-[304px] min-h-[152px] bg-neutral-0 rounded-[10px]">
           <p className="text-neutral-90 text-[16px] font-semibold">계정</p>
           <div>
-            <span className="text-neutral-70 text-[14px] font-regular">로그아웃</span>
+            <span className="text-neutral-70 text-[14px] font-regular">
+              로그아웃
+            </span>
             <button></button>
           </div>
           <div>
-            <span className="text-neutral-70 text-[14px] font-regular">회원탈퇴</span>
+            <span className="text-neutral-70 text-[14px] font-regular">
+              회원탈퇴
+            </span>
             <button></button>
           </div>
         </div>
