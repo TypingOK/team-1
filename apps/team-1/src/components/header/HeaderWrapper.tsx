f759864c4e3844c6a6b6d71969d0f82dfc99fb15
jungyeonwoo <oscar0421@kakao.com>
2024-02-07 13:16:11 +0900

    Fix: header z-index 수정

--- apps/team-1/src/components/header/HeaderWrapper.tsx
+++ apps/team-1/src/components/header/HeaderWrapper.tsx
@@ -3,7 +3,7 @@ import Header from "./Header";
 
 const HeaderWrapper = () => {
   return (
-    <div className="w-full flex flex-col items-center h-[8.25rem] border-b-neutral-5 border-b sticky -top-[2.875rem]">
+    <div className="w-full flex flex-col items-center h-[8.25rem] border-b-neutral-5 border-b sticky -top-[2.875rem] z-[10000]">
       <div className="w-full flex justify-center h-[2.875rem] items-center bg-background-5">
         <nav className="flex h-full items-center w-full max-w-[1200px]">
           <div className="w-[107px] flex justify-center">SFACFOLIO</div>
