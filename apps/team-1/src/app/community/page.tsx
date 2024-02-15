e0116268a48081cf2ae83a4e3c85150bf652735c
TypingOK <wjdwls717@gmail.com>
2024-02-15 14:36:25 +0900

    Fix: 404 나오는 페이지를 설정

---
+++ apps/team-1/src/app/community/page.tsx
@@ -0,0 +1,9 @@
+const CommunityPage = () => {
+  return (
+    <div className="w-full min-h-screen h-full flex justify-center items-center">
+      준비중 입니다.
+    </div>
+  );
+};
+
+export default CommunityPage;
